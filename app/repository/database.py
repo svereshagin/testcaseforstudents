8526e8e3cba290a30534cc77ed188590c0ac8373
Sergey <69000031+svereshagin@users.noreply.github.com>
2025-06-02 12:07:59 +0000

    ready to go with DB

---
+++ app/repository/database.py
@@ -0,0 +1,14 @@
+import psycopg2
+from ..settings import DB_CONFIG
+
+conn = psycopg2.connect(user=DB_CONFIG['user'],
+                        password=DB_CONFIG['password'],
+                        port=DB_CONFIG['port'],
+                        host=DB_CONFIG['host'],
+                        dbname=DB_CONFIG['dbname'])
+
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM cats")
+records = cur.fetchall()                    
+print(records)    
---
+++ app/settings.py
@@ -0,0 +1,11 @@
+import os
+import dotenv
+
+dotenv.load_dotenv()
+DB_CONFIG = {
+    "dbname": os.getenv("POSTGRES_DB"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "host": os.getenv("POSTGRES_HOST"),
+    "port": os.getenv("POSTGRES_PORT")
+}
