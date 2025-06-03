ec110b06d586744721e1f77a24776c6012a71079
Sergey <69000031+svereshagin@users.noreply.github.com>
2025-06-03 10:08:43 +0000

    new version of Repository

--- app/repository/database.py
+++ app/repository/database.py
@@ -1,47 +1,45 @@
 import psycopg2
 from ..settings import DB_CONFIG
 
-conn = psycopg2.connect(user=DB_CONFIG['user'],
+
+class BaseRepo():
+    def __init__(self):
+        self.conn = psycopg2.connect(user=DB_CONFIG['user'],
                         password=DB_CONFIG['password'],
                         port=DB_CONFIG['port'],
                         host=DB_CONFIG['host'],
                         dbname=DB_CONFIG['dbname'])
+        self.curr = self.conn.cursor()
 
-class BaseRepo():
-    def __get_cursor(cls):
-        return conn.cursor()
-    def ping_tables(cls):
-        # cur = cls.__get_cursor()
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM cats")
-        print(cur.fetchall())
-    def get_cats():
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM cats")
-        print(cur.fetchall())
+    def ping_tables(self):
+        self.curr.execute("SELECT * FROM cats")
+        print(self.curr.fetchall())
+
+    def get_cats(self):
+        self.curr.execute("SELECT * FROM cats")
+        print(self.curr.fetchall())
 
 
-    def create_cat(name, color, tail_length, whiskers_length):
-        with conn.cursor() as cur:
+    def create_cat(self,name, color, tail_length, whiskers_length):
+        with self.conn.cursor() as cur:
             cur.execute(
-                'INSERT INTO cats(name, color, tail_length, whiskers_length) VALUES (%s, %s, %s, %s)', 
-                (name, color, tail_length, whiskers_length)
-            )
-            conn.commit()  # Не забываем коммитить изменения
+                'INSERT INTO cats(name, color, tail_length, whiskers_length) VALUES (%s, %s, %s, %s)',  #INSERT INTO - добавить в #cats - таблица в скобках к таблице идут поля в которые 
+                                                                                                        #мы вносим данные.после VALUES #где %s %s %s %s - "экранирование слов". 
+                (name, color, tail_length, whiskers_length)                                             # аргументы которые будут подставлены(мы их передаём в функцию/метод)
+            )                                                                                           # подтвердить изменения
+            self.conn.commit()                                                                          # подтверждаем, нужно при изменении данных
+            
+    def delete_cat(self,name):
+        self.curr.execute('DELETE FROM cats WHERE name=%s', (name,))
 
-    def delete_cat(name):
-        cur = conn.cursor()
-        cur.execute('DELETE FROM cats WHERE name=(%s,)', (name))
-
-    @classmethod
-    def update_cat(cls, name, new_tail_length):
-        with conn.cursor() as cur:
-            cur.execute(
-                "UPDATE cats SET tail_length = %s WHERE name = %s", 
-                (new_tail_length, name)
-            )
-            conn.commit()
+    def update_cat(self, name, new_tail_length):
+        self.curr.execute(
+            "UPDATE cats SET tail_length = %s WHERE name = %s", 
+            (new_tail_length, name)
+        )
+        self.conn.commit()
 
 
 baseRepo = BaseRepo()
-baseRepo.update_cat(name='Musya', new_tail_length=1123123)
+baseRepo.ping_tables()
+baseRepo.update_cat(name='Vika', new_tail_length=123)
